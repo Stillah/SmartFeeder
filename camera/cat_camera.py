ab963a38de76d03d333b6b481e0ed2573394721a
Ruslan <shamil.gatiatullin@yandex.ru>
2026-04-22 23:37:44 +0300

    cat detection

---
+++ camera/cat_camera.py
@@ -0,0 +1,74 @@
+import cv2
+
+
+class CatDetectionCamera:
+    """Class to check that cat face is present in the image."""
+
+    def __init__(self, camera_index: int = 0) -> None:
+        # Start video capture from the webcam
+        self.cap = cv2.VideoCapture(camera_index, cv2.CAP_DSHOW)
+
+        self.cat_face_cascade = cv2.CascadeClassifier(
+            "haarcascade/haarcascade_frontalcatface_extended.xml"
+        )  # Use custom path for cat face cascade
+        self.smile_cascade = cv2.CascadeClassifier(
+            "haarcascade/haarcascade_smile.xml"
+        )  # Use custom path for smile cascade
+
+        self._curr_frame = self.cap.read()[1]
+
+    def check_cat_presence(self) -> bool:
+        """Check if the image contains exactly 1 cat face."""
+        # Read from the cam
+        ret, self._curr_frame = self.cap.read()
+
+        # Convert the frame to grayscale (Haar cascades work on grayscale images)
+        self.gray = cv2.cvtColor(self._curr_frame, cv2.COLOR_BGR2GRAY)
+
+        # Detect cat faces in the image
+        self.cat_faces = self.cat_face_cascade.detectMultiScale(
+            self.gray, scaleFactor=1.3, minNeighbors=5
+        )
+
+        # Exactly one cat in the image
+        return len(self.cat_faces) == 1
+
+    def get_image(self) -> tuple[bool, bytes]:
+        """Get last taken image."""
+        # Convert RGB to BGR for correct colors
+        bgr_array = cv2.cvtColor(self._curr_frame, cv2.COLOR_RGB2BGR)
+        success, encoded_img = cv2.imencode(".jpg", bgr_array)
+
+        return (success, encoded_img.tobytes())
+
+    def display(self) -> None:
+        """Display last taken image with cat's face on it."""
+        # Loop through all cat faces detected
+        # Usually should be one face
+        for x, y, w, h in self.cat_faces:
+            # Draw rectangle around the cat face
+            cv2.rectangle(self._curr_frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+            # Region of interest for detecting smiles (mouth area)
+            roi_gray = self.gray[y : y + h, x : x + w]
+            roi_color = self._curr_frame[y : y + h, x : x + w]
+
+            # Detect smiles in the cat face region
+            smiles = self.smile_cascade.detectMultiScale(roi_gray, 1.8, 20)
+
+            # Loop through all smiles detected
+            for sx, sy, sw, sh in smiles:
+                # Draw rectangle around the smile
+                cv2.rectangle(roi_color, (sx, sy), (sx + sw, sy + sh), (0, 255, 0), 2)
+
+        # Display the resulting frame
+        cv2.imshow("Cat Face and Smile Detection", self._curr_frame)
+
+    def check_close_display(self) -> bool:
+        """Break the loop when the user presses 'q'."""
+        return cv2.waitKey(1) & 0xFF == ord("q")
+
+    def stop(self) -> None:
+        """Release the webcam and close the window."""
+        self.cap.release()
+        cv2.destroyAllWindows()
---
+++ camera/cat_face_detection.py
@@ -0,0 +1,114 @@
+import argparse, requests, logging
+from datetime import datetime, timedelta
+from requests.exceptions import HTTPError, RequestException
+from weights import Weights
+from cat_camera import CatDetectionCamera
+
+logger = logging.Logger("Camera")
+url = "http://localhost/internal/logs/"
+FRAMES_IN_CYCLE = 30
+FRAMES_THRESHOLD = 20
+INTERVAL_SEC = 1
+
+
+def send_request(user_id: str, images: list[bytes], food_weight: float) -> None:
+    """Send data to the backend about the cat and its food eaten."""
+    params = {"user_id": user_id, "food_weight": food_weight}
+    files = []
+    for img in images:
+        files.append(("files_batch", img))
+
+    try:
+        response = requests.post(url, params=params, files=files)
+        image_ids = response.json()
+        logging.info(f"Scucessfully sent images. Image_ids: {image_ids}")
+    except HTTPError as e:
+        # Server returned an error (e.g., 500, 422, 404)
+        logger.warning(f"HTTP error {e.response.status_code}: {e.response.text}")
+    except RequestException as e:
+        # Network errors, timeouts, etc.
+        logging.warning(f"Request failed: {e}")
+
+
+def start_camera(user_id: str, display: bool = False) -> None:
+    """Constantly check the camera to detect cat presence and send info about food weight to backend."""
+
+    camera = CatDetectionCamera()
+    weights = Weights(port="/dev/ttyUSB0", baudrate=115200, timeout=0.1)
+
+    last = None
+    prev_weight = None
+    cat_is_visible = False
+
+    cnt_cats = 0
+    frame_number = 1
+    
+    images = []
+
+    while True:
+        weight_delta = None
+        current_weight = weights.get_weight()
+        prev_cat_here = cat_is_visible
+
+        # Exactly one cat
+        cnt_cats += camera.check_cat_presence()
+
+        if frame_number == FRAMES_IN_CYCLE:
+            if cnt_cats >= FRAMES_THRESHOLD:
+                cat_is_visible = True
+            else:
+                cat_is_visible = False
+                # Cat finished eating
+                if prev_weight and current_weight and prev_cat_here:
+                    weight_delta = current_weight - prev_weight
+                    send_request(
+                        user_id=user_id, images=images, food_weight=weight_delta
+                    )
+                    images = []
+                if current_weight:
+                    prev_weight = current_weight
+
+        # save image to send to the API
+        if cat_is_visible and (
+            not last or datetime.now() - last > timedelta(seconds=INTERVAL_SEC)
+        ):
+            success, img = camera.get_image()
+            if not success:
+                logger.warning("Failed to encode image")
+                raise RuntimeError("Failed to encode image")
+
+            images.append(img)
+            last = datetime.now()
+
+        if frame_number == FRAMES_IN_CYCLE:
+            cnt_cats = 0
+        if display:
+            camera.display()
+        if camera.check_close_display():
+            break
+        frame_number %= FRAMES_IN_CYCLE + 1
+
+    camera.stop()
+
+
+if __name__ == "__main__":
+    """
+    Usage: python cat_face_detection.py
+
+    Flags:
+    -display to display the cam
+    --user_id <user_id> to use a specific user_id
+    """
+    parser = argparse.ArgumentParser(prefix_chars="-")
+    parser.add_argument(
+        "-display", action="store_true", help="Display what camera sees"
+    )
+    parser.add_argument(
+        "--user_id",
+        type=str,
+        default="123e4567-e89b-12d3-a456-426614174000",
+        help="User UUID",
+    )
+    args = parser.parse_args()
+
+    start_camera(args.display, args.user_id)
