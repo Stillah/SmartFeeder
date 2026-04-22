2f6c1c53a17761cda466984aa3fc5b52038b34e9
nikita <svzin233@yandex.ru>
2026-04-22 14:41:02 +0300

    microcontroller code

---
+++ ML/camera/weights.py
@@ -0,0 +1,25 @@
+import serial
+import struct
+
+
+class Weights:
+
+    def __init__(
+        self, port: str, baud_rate: int, timeout: float = 0.1, bytes_in_data: int = 4
+    ):
+        self.ser = serial.Serial(port, baud_rate, timeout=timeout)
+        self.BYTES_IN_DATA = bytes_in_data
+
+    def get_weight(self) -> float | None:
+        if self.ser.in_waiting > self.BYTES_IN_DATA:
+            ready_to_get = self.ser.read(1)
+
+            if (
+                ready_to_get == b"\xaa"
+            ):  # Byte that detemines that next 4 bytes are weight value
+                raw_data = self.ser.read(self.BYTES_IN_DATA)
+
+                if len(raw_data) == self.BYTES_IN_DATA:
+                    weight = struct.unpack(">f", raw_data)[0]
+                    return weight
+        return None
